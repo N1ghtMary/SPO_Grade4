273eabb0cda9f6055f6212a6e837d784f1eb52f7
night <serf1game13@gmail.com>
2024-09-20 13:31:52 +0300

    Add LR2 6-8

--- Практика 2/app.js
+++ Практика 2/app.js
@@ -101,3 +101,65 @@ function task5Math()
                                 /(Math.sin(Math.PI + 8 * userX) * ctg( ((7 * Math.PI) / 3) + userX)) - 12 * userX;
     alert(`Result: ${mathExpressionResult}`);
 }
+
+/*
+    Task 6
+*/
+
+function task6RewriteDaycount()
+{
+    var userMonth = parseInt(document.getElementById('task6Daycount').value);
+    if(isNaN(userMonth) || userMonth < 1 || userMonth > 12)
+    {
+        alert('Number must be from 1 to 12');
+        return;
+    }
+    var days;
+    if (userMonth === 2) days = '28 или 29';
+    else if ((userMonth % 2 === 1 && userMonth <= 7) 
+            || (userMonth % 2 === 0 && userMonth > 7)) 
+    {
+        days = '31'; // Jan, Mar, May, Jul, Aug, Oct, Dec
+    } 
+    else days = '30'; // Apr, Jun, Sep, Nov
+    alert(`Amount of days in ${userMonth} month: ${days}`);
+}
+
+/*
+    Task 7
+*/
+
+function task7SwitchMonth()
+{
+    var userMonth = parseInt(document.getElementById('task7Switch').value);
+    var days;
+    switch(userMonth)
+    {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            days = '31';
+            break;
+        case 4: case 6: case 9: case 11:
+            days = '30';
+            break;
+        case 2:
+            days = '28 or 29';
+            break;
+        default:
+            days = 'Wrong month';
+    }
+    alert(`Amount of days in ${userMonth} month: ${days}`);
+}
+
+/*
+    Task 8
+*/
+
+function task8ResetBackgroundColor()
+{
+    document.body.style.backgroundColor = '#fae1c7';
+}
+
+function task8ChangeBackgroundColor(td)
+{
+    document.body.style.backgroundColor = window.getComputedStyle(td).backgroundColor;
+}
